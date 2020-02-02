2170d7aabd01e385dbc9da7fc1a14c1f560e0959
autumnchris <autumncbullard@gmail.com>
2020-02-02 14:32:16 -0500

    Set up Node.js server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {page: 'Page not found'});
+});
+
+app.listen(port, console.log(`Server is listening at port ${port}.`));
+
+module.exports = app;
