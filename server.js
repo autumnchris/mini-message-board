ae23f2614f9b84fa699d1ec08c3a0984d85e60ce
autumnchris <autumncbullard@gmail.com>
2020-02-13 01:58:02 -0500

    Set up the index route and the new-message route

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const indexRouter = require('./routes/index');
+const newMsgRouter = require('./routes/new-message');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,8 +10,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(`${__dirname}/public`));
 
+app.use('/', indexRouter);
+app.use('/new', newMsgRouter);
+
 app.use((req, res, next) => {
-    res.status(404).render('404', {page: 'Page not found'});
+    res.status(404).render('404', {title: 'Page not found'});
 });
 
 app.listen(port, console.log(`Server is listening at port ${port}.`));
