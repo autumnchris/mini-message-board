9562e477971a11c541d88563d0ab7358b7645f70
autumnchris <autumncbullard@gmail.com>
2020-02-16 02:36:40 -0500

    Display sample messages

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const dummyData = [
+  {
+    username: 'C-3PO',
+    message: 'We\'re doomed!',
+    timestamp: new Date().toLocaleString()
+  },
+  {
+    username: 'Obi-Wan Kenobi',
+    message: 'Hello there!',
+    timestamp: new Date().toLocaleString()
+  },
+  {
+    username: 'Leia Organa',
+    message: 'Help me, Obi-Wan Kenobi. You\'re my only hope.',
+    timestamp: new Date().toLocaleString()
+  }
+];
 
 router.get('/', (req, res, next) => {
-  res.render('index', {title: ''});
+  res.render('index', {title: '', messages: dummyData});
 });
 
 module.exports = router;
