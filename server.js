bfdd82abc2164264e05f03a7fc46f60b6ac01808
autumnchris <autumncbullard@gmail.com>
2020-03-04 20:30:46 -0500

    Update 404 page

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {title: 'Page not found'});
+    res.status(404).render('404', {title: 'Page not found', mainHeading: '404: Page not found'});
 });
 
 app.listen(port, console.log(`Server is listening at port ${port}.`));
